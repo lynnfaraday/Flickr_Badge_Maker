a76fce32554ac7d6ca37ac41ec2d9637e5697c26
Chris Geihsler <chris@geihsler.net>
2012-05-30 23:24:49 -0400

    added convenience methods to top level module

--- lib/flickr_badge_maker.rb
+++ lib/flickr_badge_maker.rb
@@ -2,4 +2,18 @@ require 'flickr_badge_maker/flickr_badge_maker.rb'
 require 'flickr_badge_maker/client.rb'
 
 module FlickerBadgeMaker
+  def self.do_something
+    maker.do_something
+  end
+
+  def self.do_something_else
+    maker.do_something
+  end
+
+  private
+
+  def self.maker
+    config = parse_config
+    Maker.new(config)
+  end
 end
